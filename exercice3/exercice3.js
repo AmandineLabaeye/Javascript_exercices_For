a6d26ce3b8d6fdfee3265dedfaf0c7c650b8dce5
sstienface <s.stien@fondationface.org>
2018-10-18 15:42:47 +0200

    initial commit

---
+++ exercice3/exercice3.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+
+
+/* Modifier le code ci dessous pour réaliser l'exercice */
+
+for( var i =35 ;  > ; )
+{
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
+}
+
+
